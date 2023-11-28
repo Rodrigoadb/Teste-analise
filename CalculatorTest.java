e0ce509ef61a79ac9f21158bac51d328f6e5af09
Rodrigo Alves <129088203+Rodrigoadb@users.noreply.github.com>
2023-11-28 19:38:20 -0300

    Add Junit and DOC

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -1,37 +1,75 @@
 package junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
 
-    public Calculator calc = new Calculator();
-
-
-    /*Testando a Soma */
     @Test
-    public void soma(){
-        assertEquals(2, calc.add(1, 1));
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+        assertEquals(-1, calculator.add(-4, 3));
+        
     }
 
-    /*Testando a Subtração */
     @Test
-    public void subtracao(){
-        int resultado = calc.diminuir(8,2);
-        assertEquals(6, resultado);
+    public void testDiminuir() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.diminuir(5, 3));
+        assertEquals(-7, calculator.diminuir(-4, 3));
+       
     }
 
-    /*Testando a multiplicacão */
     @Test
-    public void multiplicacao(){
-        assertEquals(25, calc.mult(5,5));
+    public void testMult() {
+        Calculator calculator = new Calculator();
+        assertEquals(15, calculator.mult(5, 3));
+        assertEquals(-12, calculator.mult(4, -3));
+     
     }
 
-    /*Testando a divisão */
     @Test
-    public void divisao(){
-        assertEquals(4, calc.div(8,2));
+    public void testDiv() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.div(6, 3));
+        assertEquals(-2, calculator.div(6, -3));
+      
     }
+
+    @Test
+    public void testDivisaoPorZero() {
+        Calculator calculator = new Calculator();
+        try {
+            calculator.div(10, 0);
+            fail("Deveria lançar uma exceção de divisão por zero.");
+        } catch (ArithmeticException e) {
+           
+        }
+    }
+
+    @Test
+    public void testOperacoesComNumerosNegativos() {
+        Calculator calculator = new Calculator();
+        assertEquals(-5, calculator.add(-2, -3));
+        assertEquals(-1, calculator.diminuir(-4, -3));
+        assertEquals(15, calculator.mult(-5, -3));
+        assertEquals(2, calculator.div(-6, -3));
+    }
+
+
+    @Test
+    public void testGrandesNumeros() {
+        Calculator calculator = new Calculator();
+        assertEquals(500000000, calculator.add(200000000, 300000000));
+        assertEquals(-100000000, calculator.diminuir(-40000000, 60000000));
+        assertEquals(1500000000, calculator.mult(5000000, 300));
+        assertEquals(2, calculator.div(60000000, 30000000));
+    }
+
+   
 
 }
+
